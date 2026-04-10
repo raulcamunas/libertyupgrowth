be05f6ee69116b74471cbf2964521aea3ee0feae
raulcamunas <raulcamunas369@gmail.com>
2026-04-10 11:06:17 +0200

    pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import '../styles/globals.css'
 import CookieBanner from '@/components/CookieBanner'
 import WhatsAppWidget from '@/components/WhatsAppWidget'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || '1222383833301915'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://libertyupgrowth.es'),
@@ -151,11 +154,40 @@ export default function RootLayout({
             referrerPolicy="no-referrer"
           />
         </noscript>
+
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${metaPixelId}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {children}
         <CookieBanner />
         <WhatsAppWidget />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         
         {/* Structured Data (JSON-LD) for SEO */}
         <script
